95240e24b701152c4f45e78b8d33f11efbf2b4a0
lags2022 <lguzman.58erb@outlook.com>
2023-02-11 14:03:58 -0500

    update 4

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 import style from "./Nav.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars  } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = (props) => {
   return (
     <header>
       <div className={style.links}>
+        <FontAwesomeIcon className={style.icon} icon={faBars } />
         <Link className={style.link} to="home">
           Home
         </Link>
