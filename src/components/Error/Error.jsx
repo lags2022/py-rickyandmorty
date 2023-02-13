62e6800b37844a28fca144b6885467bf1d481ee7
lags2022 <lguzman.58erb@outlook.com>
2023-02-13 12:48:57 -0500

    update 5

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Error = () => {
   const navigate = useNavigate();
   return (
+    //no-background esta en App.css
     <div className="no-background">
       <h1 className={style.error_h1}>44</h1>
       <button onClick={() => navigate("/home")} className={style.error_button}>
